da8faa74cb5a31ff8d75cae0f175e5313347c35d
Ben Townsend <RBT000011@gmail.com>
2021-11-03 19:46:14 -0500

    Got the server listening and added views

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
 class User extends Model {
@@ -8,7 +9,6 @@ class User extends Model {
 }
 
 User.init(
-  //
   {
     id: {
       type: DataTypes.INTEGER,
@@ -16,7 +16,7 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    username: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -32,11 +32,10 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8, 24],
+        len: [8],
       },
     },
   },
-
   {
     hooks: {
       // Use the beforeCreate hook to work with data before a new instance is created
@@ -51,22 +50,22 @@ User.init(
         return updatedUserData;
       },
       beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 11);
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
       beforeUpdate: async (updatedUserData) => {
         updatedUserData.password = await bcrypt.hash(
           updatedUserData.password,
-          11
+          10
         );
         return updatedUserData;
       },
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: "user",
     },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
   }
 );
 
