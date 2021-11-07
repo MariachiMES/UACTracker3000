2adcf14a2887cc8b4840c0927ee02b8e1d6660d4
Ben Townsend <RBT000011@gmail.com>
2021-11-07 15:59:29 -0600

    something

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -22,7 +22,7 @@ router.post("/login", async (req, res) => {
 
     if (!userData) {
       res
-        .status(400)
+        .status(401)
         .json({ message: "Incorrect email or password, please try again" });
       return;
     }
@@ -31,7 +31,7 @@ router.post("/login", async (req, res) => {
 
     if (!validPassword) {
       res
-        .status(400)
+        .status(401)
         .json({ message: "Incorrect email or password, please try again" });
       return;
     }
